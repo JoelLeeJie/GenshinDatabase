37e974219a4ef700db9cb722e4cff77d685a2407
Joel Lee <OnWingsOfAir@gmail.com>
2023-01-16 19:25:30 +0800

    Finally a breakthrough

--- GenshinDB-WPF/MainWindow.xaml.cs
+++ GenshinDB-WPF/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data;
+using System.Data.SqlClient;
+using Npgsql;
 namespace GenshinDB_WPF
 {
     /// <summary>
@@ -20,9 +22,39 @@ namespace GenshinDB_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        public NpgsqlConnection GetConnection()
+        {
+            string connectionstring = @"Server=containers-us-west-53.railway.app;Port=7418;Database=railway;User Id=dbuser;Password=genshin123;";
+            return new NpgsqlConnection(connectionstring);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultBox.Text = "";
+
+            using (NpgsqlConnection dbconnection = GetConnection())
+            {
+                dbconnection.Open(); //opens sql connection
+                using (NpgsqlCommand command1 = new NpgsqlCommand("Select * from artifactstats", dbconnection))
+                {//creates command
+                    using (NpgsqlDataReader reader = command1.ExecuteReader())
+                    {//runs command, and returns reader object.
+                        while(reader.Read()) //while there's still more rows, shift reader to next row.
+                        {
+                            ResultBox.Text = ResultBox.Text + reader[0].ToString();//uses the reader object to read the specified column. Does not read the whole row.
+                            ResultBox.Text = ResultBox.Text + reader[1].ToString() + "\n";
+                        }
+                        Console.WriteLine(ResultBox.Text);
+                    }
+                }
+                    
+            }
+            
+        }
+
     }
 }
