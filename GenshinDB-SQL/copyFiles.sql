0f0a20d09731741f95e616ea9b42789acf3c022c
Joel Lee <OnWingsOfAir@gmail.com>
2023-01-22 15:19:10 +0800

    Delete GenshinDB-SQL directory

--- GenshinDB-SQL/copyFiles.sql
+++
@@ -1,8 +0,0 @@
-copy characterinfo from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/characterinfo.csv' delimiter ',' csv header quote'"';
-copy characterconstellations from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/characterconstellations.csv' csv header quote '"';
-copy weaponinfo from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/weaponinfo.csv' csv header quote '"';
-copy weaponrefinements from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/weaponrefinements.csv' csv header quote '"';
-copy artifactsets from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/artifactsets.csv' csv header quote '"';
-copy artifactstats from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/artifactstats.csv' csv header quote '"';
-copy characterstats from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/characterstats.csv' csv header quote '"';
-copy weaponstats from 'D:/GenshinDB/GenshinDB-RawData/CsvFIles(CSV)/weaponstats.csv' csv header quote '"';
